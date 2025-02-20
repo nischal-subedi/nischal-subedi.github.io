dfa80ae300882e43b7b3c3883f723cb54f7ccb6e
Nischal Subedi <46151603+nischal-subedi@users.noreply.github.com>
2025-02-20 07:15:03 +0545

    Update homepage description
    Remove existing docusaurus placeholder, add custom description.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Learnings and Writings">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
